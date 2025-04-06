7eb9d6b61a4ae17704479571944bb11c5702d73e
Sharon S <u2104064@rajagiri.edu.in>
2025-04-06 20:11:13 +0530

    Alert system phase 2

--- models.py
+++ models.py
@@ -23,15 +23,3 @@ class Donation(db.Model):
     donor_name = db.Column(db.String(100), nullable=False)
     donation_type = db.Column(db.String(50), nullable=False)
     status = db.Column(db.String(50), default="Pending")
-
-class SelectedLocation(db.Model):
-    __tablename__ = 'selected_locations'
-    __table_args__ = {'extend_existing': True}
-
-    id = db.Column(db.Integer, primary_key=True)
-    city = db.Column(db.String(100), nullable=False, unique=True)
-    latitude = db.Column(db.Float, nullable=False)
-    longitude = db.Column(db.Float, nullable=False)
-    last_checked = db.Column(db.DateTime, default=datetime.utcnow)
-    flood_risk = db.Column(db.String(50), default="Safe")  # Safe or Unsafe
-    notify_user = db.Column(db.Boolean, default=False)  # Track new alerts
